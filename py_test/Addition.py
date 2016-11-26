ca6a5f51a8d06b0652d821c7893c1e1f1ea3ab3e
Marlon Monzon <moonrock650@gmail.com>
2016-11-26 19:04:23 +0800

    My first PyDev project

---
+++ py_test/Addition.py
@@ -0,0 +1,7 @@
+'''
+Created on Nov 26, 2016
+
+@author: Marlon_2
+'''
+
+print ("1 + 2 is : " + str(1 + 2));
---
+++ py_test/NewPythonSource.py
@@ -0,0 +1,8 @@
+'''
+Created on Nov 26, 2016
+
+@author: Marlon_2
+'''
+
+if __name__ == '__main__':
+    pass
