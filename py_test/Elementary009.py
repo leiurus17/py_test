d801d51d9c4854d453cb40d923d1aba1153b6821
Marlon Monzon <moonrock650@gmail.com>
2016-12-05 20:53:12 +0800

    Loop01.py and Elementary009.py

---
+++ py_test/Elementary009.py
@@ -0,0 +1,18 @@
+'''
+Created on Dec 3, 2016
+
+@author: Marlon_2
+'''
+# Write a guessing game where the user has to guess a secret number.
+# After every guess the program tells the user whether their number was too large or too small.
+# At the end the number of tries needed should be printed.
+# I counts only as one try if they input the same number multiple times consecutively.
+
+import random
+
+number_to_guess = random.randrange(0,50);
+
+print("Number Guessing Game");
+print("Guess the number between")
+
+print(number_to_guess);
---
+++ py_test/Loop01.py
@@ -0,0 +1,13 @@
+'''
+Created on Dec 5, 2016
+
+@author: Marlon_2
+'''
+for num in range(1,7):
+    print(num);
+    
+    
+print("=====");    
+    
+for num in range(1,70,2):
+    print(num);
