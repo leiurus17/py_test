b877c35e5f64cae790be88a0ba6666d5bc0f9883
Marlon Monzon <moonrock650@gmail.com>
2017-01-15 15:54:32 +0800

    Configure the button to be re-clickable with new event

---
+++ py_test/py_test/gui/hello_gui_2.py
@@ -0,0 +1,42 @@
+'''
+Created on Jan 15, 2017
+
+@author: Marlon_2
+'''
+import tkinter as tk # import
+from tkinter import ttk   # impork ttk from tkinter
+
+win = tk.Tk();       # create instance
+
+#add a title
+win.title("Python Hospital Information System");
+
+#add a label
+#ttk.Label(win, text="Welcome to Python Hospital Information System").grid(column=0,row=0);
+
+aLabel = ttk.Label(win, text="Welcome to Python Hospital Information System");
+aLabel.grid(column=0,row=0);
+
+def clickMe():
+    action.configure(text="** I have been clicked **")
+    aLabel.configure(foreground='red',background='yellow')
+    action.configure(command=clickMeReset)
+    
+def clickMeReset():
+    action.configure(text="** Click Me! **")
+    aLabel.configure(foreground='black',background='white')
+    action.configure(command=clickMe)
+
+#Adding a button
+action = ttk.Button(win, text="Click Me!", command=clickMe)
+action.grid(column=0, row=1);
+
+
+    
+
+#button click event callback function
+
+
+#win.resizable(200, 100); # disable resizing of the GUI
+
+win.mainloop(); # start GUI
