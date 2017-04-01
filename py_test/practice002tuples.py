3e072117d08d728fadff1d363515cfb3cedc1736
Marlon Monzon <moonrock650@gmail.com>
2017-04-01 12:22:10 +0800

    Tuples Practice

---
+++ py_test/practice002tuples.py
@@ -0,0 +1,17 @@
+'''
+Created on Apr 1, 2017
+
+Tuples
+
+@author: Marlon_2
+'''
+
+blank_tuple = ()
+single_tuple = ('naruto',)
+
+print(single_tuple)
+
+#This will not work, as tuples are not changeable
+#single_tuple('naruto', 'hinata')
+
+print(single_tuple)
