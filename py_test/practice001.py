2208c5cc924777edec8f6da091f645248f28841d
Marlon Monzon <moonrock650@gmail.com>
2017-04-01 11:05:10 +0800

    practice001.py - Lists

---
+++ py_test/practice001.py
@@ -0,0 +1,23 @@
+'''
+Created on Apr 1, 2017
+
+@author: Marlon_2
+'''
+
+fruits = ['avocados', 'apple', 'banana', 'oranges', 'grapes']
+print(fruits[0])
+print(fruits[0:2])
+print(fruits[2:5])
+
+print('mango' in fruits)
+print('apple' in fruits)
+
+print('ana' in 'banana')
+print('wow' in 'mango')
+
+print('pine' + 'apple')
+
+print('heart ' * 5)
+
+print(", ".join(fruits))
+print(fruits)
